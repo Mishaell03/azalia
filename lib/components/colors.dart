23b830d1489125c316a014e5fb12409d170b61c4
Mishaell03 <zaloginmisha2008@gmail.com>
2025-11-01 00:04:33 +0300

    add standard colors + icons

---
+++ lib/components/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const brown = Color(0xFFD3B398);
+  static const star = Color(0xFFFFBB56);
+  static const white = Color(0xFFFFFFFF);
+  static const white_dark = Color(0xFFF4F4F4);
+  static const black = Color(0xFF000000);
+  static const black_transparent = Color(0x80000000);
+  static const grey = Color(0xFF505050);
+  static const grey_light = Color(0xFFBFC2C8);
+}
