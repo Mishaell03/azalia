fe9b500ac957835b37c5c2d31972971723b374b9
Mishaell03 <zaloginmisha2008@gmail.com>
2025-11-01 00:22:52 +0300

    add standart text styles

---
+++ lib/components/text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppText {
+
+  static const bold_28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+  );
+  static const bold_15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+  );
+  
+
+  static const semibold_28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+  );
+  static const semibold_15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+  );
+  static const semibold_13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+  );
+  static const semibold_12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+  
+  static const medium_24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+  static const medium_16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const medium_13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+  static const medium_12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+}
