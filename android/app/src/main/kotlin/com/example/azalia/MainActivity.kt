83fa7310fd7cd24afbc3b5e6194a9662ca1e9537
Mishaell03 <zaloginmisha2008@gmail.com>
2025-10-31 23:25:27 +0300

    init project

---
+++ android/app/src/main/kotlin/com/example/azalia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.azalia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+// временная заглушка
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
